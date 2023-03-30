5a3ac12a91a5c717e7e46f8c63d4b0e5e150c759
kozlu14 <105872690+kozlu14@users.noreply.github.com>
2023-03-30 16:56:28 +0300

    Garlic for havoc 11

--- havoc_garlic.mk
+++ havoc_garlic.mk
@@ -1,17 +1,7 @@
 #
-# Copyright (C) 2022 Lineageos
+# Copyright (C) 2018-2021 Android Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
@@ -24,13 +14,14 @@ $(call inherit-product, device/yu/garlic/device.mk)
 # Inherit from common device
 $(call inherit-product, device/tinno/msm8937-common/msm8937.mk)
 
-# Inherit some CesiumOS stuff
-$(call inherit-product, vendor/cesium/config/common_full_phone.mk)
+# Inherit common AOSP configurations
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := garlic
-PRODUCT_NAME := cesium_garlic
+PRODUCT_NAME := havoc_garlic
 PRODUCT_BRAND := yu
 PRODUCT_MODEL := YU Yureka Black
 PRODUCT_MANUFACTURER := yu
